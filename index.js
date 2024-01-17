f98d90e9911082573ef23f938b99fe2dccbef6e4
Dale Warburton <dalewarby23@outlook.com>
2024-01-17 02:45:06 +0000

    Started variables in JS and also decided to add a clear history button which will have a function to clear the save data from the storage

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 //Generated the API Key for the weather application
 const APIKey = "98fcbdcfb0b3d080b76818012068c994";
+
+//Storing variable to store the searched City
+let City = "";
+
+//list of variables to be defined
+let searchCity = $("#search-input");
+let searchButton = $("#search-button");
+let clearSearch = $("")
