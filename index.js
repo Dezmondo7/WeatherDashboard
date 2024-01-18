3b49af0d173ab66316fa2fc51dc61d46c5cea2cc
Dale Warburton <dalewarby23@outlook.com>
2024-01-18 14:44:34 +0000

    added bootstrap version 4 and CDN, also added the table for the Temp, humidity and windspeed for the current weather and also the five day weather forcast below.

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 //Generated the API Key for the weather application
 const APIKey = "98fcbdcfb0b3d080b76818012068c994";
 
-//Storing variable to store the searched City
+//Variable to store the searched City
 let City = "";
 
 //list of variables to be defined
 let searchCity = $("#search-input");
 let searchButton = $("#search-button");
-let clearSearch = $("")
+let clearSearch = $("#clear-search");
+let currentCity = $("#current-city");
+let currentTemperature = $("#current-temperature")
+
+
+
